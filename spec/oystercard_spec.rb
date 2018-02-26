f1be8f0770ddb67f86974d85b6acca12f1ac203e
Vivian Allen <mrvivianallen@gmail.com>
2018-02-26 14:50:21 +0000

    balance feature added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,13 @@
+require 'oystercard'
+
 describe Oystercard do
+
+  subject(:oystercard) { described_class.new }
+
+  describe '#balance' do
+    it 'initializes with a balance of zero' do
+      expect(oystercard.balance).to eq 0
+    end
+  end
+
 end
