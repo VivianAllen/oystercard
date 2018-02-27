f0f26b1edd9e85810bf84a204d57e6d0908e05ca
Vivian Allen <mrvivianallen@gmail.com>
2018-02-27 22:15:27 +0000

    added dynamic balance based on journey history

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,20 +3,18 @@ class Oystercard
   MAXIMUM_BALANCE = 90
   MINIMUM_FARE = 1
 
-  attr_reader :balance
-
   def initialize(journey_log_class = JourneyLog)
-    @balance = 0.0
     @journey_log = journey_log_class.new
+    @top_up_sum = 0.0
   end
 
   def top_up(amount)
-    raise Exception.new("Maximum balance of £#{MAXIMUM_BALANCE} exceeded!") if @balance + amount > MAXIMUM_BALANCE
-    @balance += amount
+    raise Exception.new("Maximum balance of £#{MAXIMUM_BALANCE} exceeded!") if balance + amount > MAXIMUM_BALANCE
+    @top_up_sum += amount
   end
 
   def touch_in(station)
-    raise Exception.new("Minimum balance for travel is £#{MINIMUM_FARE}") if @balance < MINIMUM_FARE
+    raise Exception.new("Minimum balance for travel is £#{MINIMUM_FARE}") if balance < MINIMUM_FARE
     @journey_log.start(station)
   end
 
@@ -28,10 +26,18 @@ class Oystercard
     @journey_log.journeys
   end
 
+  def balance
+    @top_up_sum - sum_fares
+  end
+
   private
 
-  def deduct(fare)
-    @balance -= fare
+  def sum_fares
+    return 0.0 unless journey_history
+    summed_fares = 0.0
+    journey_history.each do |journey|
+      summed_fares += journey.fare
+    end
   end
 
 end
